cf7992075a7de5dc7f49f024f5ed10432e4d7c73
Maikudou <maikudou@gmail.com>
2017-12-07 00:05:41 +0300

    fixifix

--- 2017/day3/day3.js
+++ 2017/day3/day3.js
@@ -3,39 +3,38 @@ var width = 1;
 var height = 1;
 var value = 1;
 var direction = "R";
-var current = 0;
 
 var x = 0;
 var y = 0;
 
-while (current < input) {
+while (value < input) {
     switch(direction) {
         case "R":
-            for(var i = 0; i < width && current < input; i++) {
-                current++;
+            for(var i = 0; i < width && value < input; i++) {
+                value++;
                 x++;
             }
             width += 2;
             direction = "U";
             break;
         case "U":
-            for(var i = 0; i < height && current < input; i++) {
-                current++;
+            for(var i = 0; i < height && value < input; i++) {
+                value++;
                 y++;
             }
             height += 2;
             direction = "L";
             break;
         case "L":
-            for(var i = 0; i < width-1 && current < input; i++) {
-                current++;
+            for(var i = 0; i < width-1 && value < input; i++) {
+                value++;
                 x--;
             }
             direction = "D";
             break;
         case "D":
-            for(var i = 0; i < height-1 && current < input; i++) {
-                current++;
+            for(var i = 0; i < height-1 && value < input; i++) {
+                value++;
                 y--;
             }
             direction = "R";

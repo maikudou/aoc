b81123055abe383ceb4626c14cdc5ca33a95e750
Mikhail Labanov <maikudou@yandex-team.ru>
2017-12-10 19:16:42 +0300

    day 10 2017

---
+++ 2017/day10/day10_2.js
@@ -0,0 +1,83 @@
+var input = "106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118".split("").map(function(string){
+    return string.codePointAt();
+}).concat([17, 31, 73, 47, 23]);
+
+// input = "AoC 2017".split("").map(function(string){
+//     return string.codePointAt();
+// }).concat([17, 31, 73, 47, 23]);
+
+var nodes = [];
+for (var i = 0; i<256; i++) {
+    nodes.push(i);
+}
+
+// input = "3,4,1,5".split(",");
+// nodes = [];
+// for (var i = 0; i<5; i++) {
+//     nodes.push(i);
+// }
+
+var pos = 0;
+var replacePos;
+var skip = 0;
+var length;
+var selected;
+var change;
+
+for (var round = 0; round < 64; round++) {
+    for (var i = 0; i < input.length; i++) {
+        length = input[i];
+        if (length > nodes.length) {
+            continue;
+        }
+        change =  pos + length - nodes.length;
+        selected = nodes.slice(pos, pos + length);
+
+        if (change > 0) {
+            selected = selected.concat(nodes.slice(0, change));
+        }
+        selected.reverse();
+
+        if (pos >= nodes.length ) {
+            console.log(pos);
+        }
+
+        replacePos = pos;
+        for (var j = 0; j < selected.length; j++) {
+            if (replacePos == nodes.length) {
+                replacePos = 0;
+            }
+
+            if (replacePos >= nodes.length ) {
+                // console.log(replacePos);
+            }
+
+            nodes[replacePos] = selected[j];
+
+            replacePos++;
+        }
+
+        pos += length + skip;
+
+        while (pos >= nodes.length) {
+            pos = pos - nodes.length;
+        }
+
+        skip++;
+    }
+}
+
+var sparse = [];
+var xored;
+for (i = 0; i < 16; i++) {
+    xored = 0;
+    for (k = 0; k < 16; k++) {
+        xored = xored ^ nodes[16*i+k];
+    }
+    if (xored > 9) {
+        sparse.push(xored.toString(16));
+    } else {
+        sparse.push("0" + xored.toString(16));
+    }
+}
+console.log(sparse.join(""));

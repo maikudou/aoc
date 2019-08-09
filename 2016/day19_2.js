6c8ff91264277bd4de07ca7b422033ef1cea376d
Maikudou <maikudou@gmail.com>
2019-08-09 07:07:25 -0700

    WIP 2016 day 19

--- 2016/day19_2.js
+++ 2016/day19_2.js
@@ -1,83 +1,111 @@
-const input = 5; // 3001330;
+const input = 3001330; // 3001330;
 
-var elves = [];
+var elves = new Array(input);
 for (var i = 0; i < input; i++) {
-    elves.push(1);
+    elves[i] = { i: i + 1, p: 1 };
 }
 
+// i = 0;
+// var nextI;
+// var nextIBack;
+
+// while (true) { // eslint-disable-line
+//     // console.log(elves);
+//     while (elves[i] === 0) {
+//         i++;
+//         if (i === input) {
+//             i = 0;
+//         }
+//     }
+
+//     nextI = i + 1;
+//     if (nextI >= input - 1) {
+//         nextI = 0;
+//     }
+
+//     nextIBack = i - 1;
+//     if (nextIBack < 0) {
+//         nextIBack = input - 1;
+//     }
+
+//     var restrainer = 0;
+
+//     while (nextI !== nextIBack) {
+//         if (restrainer++ > input) {
+//             console.log(nextI, nextIBack);
+//             process.exit();
+//         }
+//         while (elves[nextI] === 0) {
+//             nextI++;
+//             if (nextI === input) {
+//                 nextI = 0;
+//             }
+//         }
+
+//         while (elves[nextIBack] === 0) {
+//             nextIBack--;
+//             if (nextIBack === -1) {
+//                 nextIBack = input - 1;
+//             }
+//         }
+
+//         if (nextI === nextIBack) {
+//             break;
+//         }
+
+//         nextI++;
+//         if (nextI >= input - 1) {
+//             nextI = 0;
+//         }
+
+//         if (nextI === nextIBack) {
+//             nextI--;
+//             if (nextI < 0) {
+//                 nextI = input - 1;
+//             }
+//             break;
+//         }
+
+//         nextIBack--;
+//         if (nextIBack < 0) {
+//             nextIBack = input - 1;
+//         }
+//     }
+
+//     // console.log('nextI', nextI);
+
+//     if (i === nextI) {
+//         break;
+//     }
+//     elves[i] += elves[nextI];
+
+//     if (elves[i] === input) {
+//         break;
+//     }
+
+//     elves[nextI] = 0;
+
+//     i++;
+//     if (i >= input) {
+//         i = 0;
+//     }
+// }
 i = 0;
-var nextI;
-var nextIBack;
+while (elves.length > 1) {
+    var nextI = i + Math.floor(elves.length / 2);
 
-var counter = 0;
-
-while (true && counter < 5) { // eslint-disable-line
-    console.log(elves);
-    while (elves[i] === 0) {
-        i++;
-        if (i === input) {
-            i = 0;
-        }
+    if (nextI >= elves.length) {
+        nextI = nextI - elves.length;
     }
 
-    nextI = i + 1;
-    if (nextI >= input - 1) {
-        nextI = 0;
-    }
-
-    nextIBack = i - 1;
-    if (nextIBack < 0) {
-        nextIBack = input - 1;
-    }
-
-    while (nextI !== nextIBack) {
-        console.log(1, nextI, nextIBack);
-        while (elves[nextI] === 0) {
-            nextI++;
-            if (nextI === input) {
-                nextI = 0;
-            }
-        }
-
-        console.log(2, nextI, nextIBack);
-
-        while (elves[nextIBack] === 0) {
-            nextIBack--;
-            if (nextIBack === -1) {
-                nextIBack = input - 1;
-            }
-        }
-
-        console.log(3, nextI, nextIBack);
-
-        nextI++;
-        if (nextI >= input - 1) {
-            nextI = 0;
-        }
-
-        nextIBack--;
-        if (nextIBack < 0) {
-            nextIBack = input - 1;
-        }
-    }
-
-    console.log('nextI', nextI);
-
-    if (i === nextI) {
-        break;
-    }
-    elves[i] += elves[nextI];
-
-    if (elves[i] === input) {
-        break;
-    }
-
-    elves[nextI] = 0;
-
+    // console.log(elves[nextI].i, i);
+    elves[i].p += elves[nextI].p;
+    elves.splice(nextI, 1);
     i++;
-    if (i >= input) {
+    if (i >= elves.length) {
         i = 0;
     }
+
 }
 
-console.log(i + 1);
+console.log(elves[0].i);

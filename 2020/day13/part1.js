1e2e373ee69a8226b4011f41f92746f2f27c1130
Mike Labanov <mike.labanov@grammarly.com>
2021-01-07 19:14:36 -0800

    Day 13 WIP

--- 2020/day13/part1.js
+++ 2020/day13/part1.js
@@ -1,7 +1,34 @@
 var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream(__dirname + '/test')
+  input: require('fs').createReadStream(__dirname + '/input')
 })
 
-lineReader.on('line', function (line) {})
+let startTime
+let busses
 
-lineReader.on('close', function () {})
+lineReader.on('line', function (line) {
+  if (!startTime) {
+    startTime = parseInt(line, 10)
+  } else {
+    busses = line.split(',').reduce((acc, value) => {
+      if (value != 'x') {
+        acc.push(parseInt(value, 10))
+      }
+      return acc
+    }, [])
+  }
+})
+
+lineReader.on('close', function () {
+  let busNum = null
+  let closest = Infinity
+  busses.map(bus => {
+    const timeSince = startTime % bus
+    const timeTo = timeSince == 0 ? 0 : bus - timeSince
+    if (timeTo < closest) {
+      busNum = bus
+      closest = timeTo
+    }
+    return timeTo
+  })
+  console.log(busNum * closest)
+})
--- 2020/day13/part2.js
+++ 2020/day13/part2.js
@@ -2,6 +2,59 @@ var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream(__dirname + '/test')
 })
 
-lineReader.on('line', function (line) {})
+let startTime
+let busses
 
-lineReader.on('close', function () {})
+lineReader.on('line', function (line) {
+  if (!startTime) {
+    startTime = parseInt(line, 10)
+  } else {
+    busses = line.split(',').reduce((acc, value) => {
+      if (value != 'x') {
+        acc.push(parseInt(value, 10))
+      } else {
+        acc.push('x')
+      }
+      return acc
+    }, [])
+  }
+})
+
+lineReader.on('close', function () {
+  let timestamp = 0
+  let iterations = 0
+  let smallIterations = 0
+  while (true) {
+    iterations++
+    let prevTo = -1
+    let i = 0
+    for (i = 0; i < busses.length; i++) {
+      const bus = busses[i]
+      if (bus == 'x') {
+        prevTo++
+      } else {
+        let timeSince = timestamp % bus
+        let timeTo = timeSince == 0 ? 0 : bus - timeSince
+        if (timeTo == prevTo + 1) {
+          prevTo++
+        } else if (timeTo > prevTo + 1) {
+          while (timeTo != prevTo + 1) {
+            smallIterations++
+            timestamp += busses[0]
+            timeSince = timestamp % bus
+            timeTo = timeSince == 0 ? 0 : bus - timeSince
+          }
+          timestamp -= busses[0]
+          break
+        } else {
+          break
+        }
+      }
+    }
+    if (i == busses.length) {
+      break
+    }
+    timestamp += busses[0]
+  }
+  console.log(timestamp, iterations, smallIterations)
+})

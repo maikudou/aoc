75d96601c73977360a5678f5011a8c329c461ce0
Mikhail Labanov <maikudou@gmail.com>
2021-01-10 18:35:25 -0800

    2020 days 15, 16

---
+++ 2020/day15/part2.js
@@ -0,0 +1,40 @@
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(__dirname + '/input')
+})
+
+let nums
+
+lineReader.on('line', function (line) {
+  nums = line.split(',').map(num => parseInt(num, 10))
+})
+
+lineReader.on('close', function () {
+  step = 1
+
+  const log = new Map()
+  let lastSpoken
+
+  while (step <= 30000000) {
+    const lastSpokenBefore = lastSpoken
+    if (nums.length) {
+      lastSpoken = nums.shift()
+      log.set(lastSpoken, [step])
+    } else {
+      let lastSpokenLog = log.get(lastSpoken) || []
+      if (lastSpokenLog.length > 1) {
+        lastSpoken = lastSpokenLog[1] - lastSpokenLog[0]
+      } else {
+        lastSpoken = 0
+      }
+      lastSpokenLog = log.get(lastSpoken) || []
+      lastSpokenLog.push(step)
+      if (lastSpokenLog.length > 2) {
+        lastSpokenLog.shift()
+      }
+      log.set(lastSpoken, lastSpokenLog)
+    }
+    step++
+  }
+
+  console.log(lastSpoken)
+})

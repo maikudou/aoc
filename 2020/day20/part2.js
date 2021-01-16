48958b6272e4b669f21a451a0da42ab5a5a65835
Mikhail Labanov <maikudou@gmail.com>
2021-01-15 20:01:07 -0800

    2020 day 20

--- 2020/day20/part2.js
+++ 2020/day20/part2.js
@@ -1,10 +1,26 @@
 var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream(__dirname + '/test')
+  input: require('fs').createReadStream(__dirname + '/input')
 })
 
 const tiles = new Map()
 const sides = new Map()
 let currentTile
+
+flipMapX = array => array.slice().reverse()
+flipMapY = array => array.map(row => row.slice().reverse())
+rotateMap = array =>
+  array.reduce((acc, value) => {
+    if (acc.length == 0) {
+      value.map(val => {
+        acc.push([val])
+      })
+    } else {
+      value.map((value, index) => {
+        acc[index].unshift(value)
+      })
+    }
+    return acc
+  }, [])
 
 // Flip along x axis
 const flipX = tile => {
@@ -16,7 +32,7 @@ const flipX = tile => {
       w: tile.sides.w.split('').reverse().join(''),
       e: tile.sides.e.split('').reverse().join('')
     },
-    map: tile.map.slice().reverse()
+    map: flipMapX(tile.map)
   }
 }
 
@@ -30,7 +46,7 @@ const flipY = tile => {
       w: tile.sides.e,
       e: tile.sides.w
     },
-    map: tile.map.map(row => row.slice().reverse())
+    map: flipMapY(tile.map)
   }
 }
 
@@ -44,18 +60,7 @@ const rotate = tile => {
       w: tile.sides.s,
       e: tile.sides.n
     },
-    map: tile.map.reduce((acc, value) => {
-      if (acc.length == 0) {
-        value.map(val => {
-          acc.push([val])
-        })
-      } else {
-        value.map((value, index) => {
-          acc[index].unshift(value)
-        })
-      }
-      return acc
-    }, [])
+    map: rotateMap(tile.map)
   }
 }
 
@@ -113,22 +118,114 @@ const fit = (tile, top, left) => {
   let goodSR = false
   let goodER = false
 
+  // console.log(tile.id, tile.sides, top, left)
+
   if (top) {
-    if (sides.get(tile.sides.n).indexOf(top) > -1) {
+    if (tile.sides.n == top) {
       goodN = true
     }
-    if (sides.get(tile.sides.s).indexOf(top) > -1) {
+    if (tile.sides.s == top) {
+      goodS = true
+    }
+    if (tile.sides.n.split('').reverse().join('') == top) {
+      goodNR = true
+    }
+    if (tile.sides.s.split('').reverse().join('') == top) {
+      goodSR = true
+    }
+  } else {
+    if (sides.get(tile.sides.n).length == 1) {
+      goodN = true
+    }
+    if (sides.get(tile.sides.s).length == 1) {
       goodS = true
     }
   }
   if (left) {
-    if (sides.get(tile.sides.w).indexOf(left) > -1) {
+    if (tile.sides.w == left) {
       goodW = true
     }
-    if (sides.get(tile.sides.e).indexOf(top) > -1) {
+    if (tile.sides.e == left) {
+      goodE = true
+    }
+    if (tile.sides.w.split('').reverse().join('') == left) {
+      goodWR = true
+    }
+    if (tile.sides.e.split('').reverse().join('') == left) {
+      goodER = true
+    }
+  } else {
+    if (sides.get(tile.sides.w).length == 1) {
+      goodW = true
+    }
+    if (sides.get(tile.sides.e).length == 1) {
       goodE = true
     }
   }
+
+  // console.log(goodN, goodW, goodS, goodE, goodNR, goodWR, goodSR, goodER)
+
+  if (goodS && (goodWR || goodW)) {
+    return flipX(tile)
+  }
+  if (goodE && goodNR) {
+    return flipY(tile)
+  }
+  if (goodSR && goodER) {
+    return rotate(rotate(tile))
+  }
+  if (goodN && goodW) {
+    return tile
+  } else {
+    return fit(rotate(tile), top, left)
+  }
+}
+
+const buildImage = map => {
+  return map.reduce((image, row) => {
+    const rowImage = row.reduce((rowImage, tile) => {
+      rowImage = tile.map.reduce((tileImage, tileRow, index, array) => {
+        if (!index || index == array.length - 1) {
+          return rowImage
+        }
+        if (!rowImage[index - 1]) {
+          rowImage[index - 1] = []
+        }
+        rowImage[index - 1] = rowImage[index - 1].concat(tileRow.slice(1, -1))
+      }, rowImage)
+      return rowImage
+    }, [])
+    image = image.concat(rowImage)
+    return image
+  }, [])
+}
+
+const findMonsters = image => {
+  let monstersCount = 0
+  for (var y = 1; y < image.length - 1; y++) {
+    for (var x = 0; x < image[0].length - 20; x++) {
+      if (
+        image[y][x] == '#' &&
+        image[y + 1][x + 1] == '#' &&
+        image[y + 1][x + 4] == '#' &&
+        image[y][x + 5] == '#' &&
+        image[y][x + 6] == '#' &&
+        image[y + 1][x + 7] == '#' &&
+        image[y + 1][x + 10] == '#' &&
+        image[y][x + 11] == '#' &&
+        image[y][x + 12] == '#' &&
+        image[y + 1][x + 13] == '#' &&
+        image[y + 1][x + 16] == '#' &&
+        image[y][x + 17] == '#' &&
+        image[y][x + 18] == '#' &&
+        image[y][x + 19] == '#' &&
+        image[y - 1][x + 18] == '#'
+      ) {
+        monstersCount++
+      }
+    }
+  }
+  return monstersCount
 }
 
 lineReader.on('line', function (line) {
@@ -168,42 +265,70 @@ lineReader.on('close', function () {
     }
   }
 
-  const finalTileMap = [[tiles.get(firstCorner)]]
+  const finalTileMap = [[fit(tiles.get(firstCorner))]]
   const side = Math.sqrt(tiles.size)
   for (var y = 0; y < side; y++) {
     for (var x = 0; x < side - 1; x++) {
-      // first iteration of a new row
       if (!finalTileMap[y]) {
-      } else {
-        let setTile = finalTileMap[y][x]
-
-        // First corner
-        if (y == 0 && x == 0) {
-          let flipCurrentX = false
-          let flipCurrentY = false
-          if (sides.has(setTile.sides.n) && sides.get(setTile.sides.n).size > 1) {
-            flipCurrentX = true
-          }
-          if (sides.has(setTile.sides.w) && sides.get(setTile.sides.w).size > 1) {
-            flipCurrentY = true
-          }
-          if (flipCurrentX) {
-            setTile = flipX(setTile)
-          }
-          if (flipCurrentY) {
-            setTile = flipY(setTile)
-          }
-          // console.log(flipCurrentX, flipCurrentY, setTile)
-        }
-
-        let nextTiles = sides.get(setTile.sides.e).filter(id => id != setTile.id)
+        let setTile = finalTileMap[y - 1][0]
+        let nextTiles = sides.get(setTile.sides.s).filter(id => id != setTile.id)
         if (nextTiles.size > 1) {
-          console.log('Something wrong!')
+          console.log('Something wrong! Too many possibilities')
           process.exit(1)
         }
-        let nextTile = tiles.get(nextTiles[0])
-        console.log(nextTile)
+        finalTileMap.push([fit(tiles.get(nextTiles[0]), setTile.sides.s)])
       }
+      let setTile = finalTileMap[y][x]
+      let nextTiles = sides.get(setTile.sides.e).filter(id => id != setTile.id)
+      if (nextTiles.length > 1) {
+        console.log('Something wrong! Too many possibilities')
+        process.exit(1)
+      }
+      if (!nextTiles.length) {
+        console.log('Something wrong! No possible tiles')
+        process.exit(1)
+      }
+      finalTileMap[y].push(
+        fit(
+          tiles.get(nextTiles[0]),
+          finalTileMap[y - 1]
+            ? finalTileMap[y - 1][x + 1]
+              ? finalTileMap[y - 1][x + 1].sides.s
+              : false
+            : false,
+          setTile.sides.e
+        )
+      )
     }
   }
+  let image = buildImage(finalTileMap)
+  let monstersCount = 0
+  const actions = [
+    rotateMap,
+    rotateMap,
+    rotateMap,
+    flipMapX,
+    rotateMap,
+    flipMapX,
+    rotateMap,
+    flipMapY,
+    rotateMap,
+    flipMapY
+  ]
+  while (monstersCount == 0 && actions.length) {
+    monstersCount = findMonsters(image)
+    let action = actions.shift()
+    image = action(image)
+  }
+  console.log(
+    image.reduce((acc, value) => {
+      return (
+        acc +
+        value.reduce((lineAcc, cell) => {
+          return lineAcc + (cell == '#' ? 1 : 0)
+        }, 0)
+      )
+    }, 0) -
+      monstersCount * 15
+  )
 })

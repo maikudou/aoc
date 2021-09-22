b7c2c9ef89b4d7a0872907a6ea0edf6e24387163
Mike Labanov <maikudou@gmail.com>
2021-09-21 17:23:54 -0700

    2017 day 19

---
+++ 2017/day19/part1.js
@@ -0,0 +1,63 @@
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(__dirname + '/input')
+})
+
+const map = []
+let y = 0
+let x = 0
+let direction = 's'
+let letters = []
+
+lineReader.on('line', function (line) {
+  map.push(line.split(''))
+})
+
+lineReader.on('close', function () {
+  let prevChar
+  while (prevChar !== '|') {
+    prevChar = map[0][x]
+    x++
+  }
+  x--
+
+  while (direction) {
+    switch (direction) {
+      case 'n':
+        y--
+        break
+      case 's':
+        y++
+        break
+      case 'e':
+        x++
+        break
+      case 'w':
+        x--
+        break
+    }
+    if (!map[y]) {
+      break
+    }
+    const currentChar = map[y][x]
+
+    if (currentChar === undefined || currentChar === ' ') {
+      break
+    }
+
+    if (currentChar === '+') {
+      switch (direction) {
+        case 'n':
+        case 's':
+          direction = map[y][x + 1] ? (map[y][x + 1] === ' ' ? 'w' : 'e') : 'w'
+          break
+        case 'e':
+        case 'w':
+          direction = map[y + 1] ? (map[y + 1][x] ? (map[y + 1][x] === ' ' ? 'n' : 's') : 'n') : 'n'
+          break
+      }
+    } else if (currentChar !== '|' && currentChar !== '-') {
+      letters.push(currentChar)
+    }
+  }
+  console.log(letters.join(''))
+})

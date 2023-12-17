c626d5a7c5713669b19136a8ebeac35e12b13c5f
Mikhail Labanov <maikudou@gmail.com>
2023-12-16 22:59:40 -0800

    2023 day 9

---
+++ 2023/day09/part1.js
@@ -0,0 +1,35 @@
+const { diff } = require('prettier')
+const toDecimal = require('../../utils/toDecimal')
+
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(__dirname + '/input')
+})
+
+let sum = 0
+
+lineReader.on('line', function (line) {
+  const diffs = [line.split(' ').map(toDecimal)]
+  let diff = null
+  while (true) {
+    diff = diffs[diffs.length - 1].reduce((acc, value, index, array) => {
+      if (index) {
+        acc.push(array[index] - array[index - 1])
+      }
+      return acc
+    }, [])
+
+    if (diff.findIndex(d => d !== 0) === -1) {
+      break
+    }
+    diffs.push(diff)
+  }
+  // console.log(diffs)
+  sum += diffs.reduceRight((acc, value) => {
+    acc += value[value.length - 1]
+    return acc
+  }, 0)
+})
+
+lineReader.on('close', function () {
+  console.log(sum)
+})

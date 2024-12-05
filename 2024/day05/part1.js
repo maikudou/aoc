50314ea319832b82e950efe35be05d08e59c10df
Mikhail Labanov <maikudou@gmail.com>
2024-12-05 07:43:22 -0800

    2024 day 5

---
+++ 2024/day05/part1.js
@@ -0,0 +1,51 @@
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(__dirname + '/input')
+})
+
+var rulesDone = false
+const pageSets = []
+const rules = []
+
+lineReader.on('line', function (line) {
+  if (!line) {
+    rulesDone = true
+    return
+  }
+  if (rulesDone) {
+    pageSets.push(line.split(','))
+  } else {
+    rules.push(line.split('|'))
+  }
+})
+
+lineReader.on('close', function () {
+  const rulesProcessed = new Map()
+  rules.forEach(([target, after]) => {
+    if (!rulesProcessed.has(after)) {
+      rulesProcessed.set(after, new Set([target]))
+    } else {
+      rulesProcessed.get(after).add(target)
+    }
+  })
+  let sum = 0
+  pageSets.forEach(pages => {
+    const shouldNotBeThere = new Set()
+    if (
+      pages.every(page => {
+        if (rulesProcessed.has(page)) {
+          Array.from(rulesProcessed.get(page).values()).forEach(pageShouldBeBefore => {
+            shouldNotBeThere.add(pageShouldBeBefore)
+          })
+        }
+        if (shouldNotBeThere.has(page)) {
+          return false
+        } else {
+          return true
+        }
+      })
+    ) {
+      sum += parseInt(pages[Math.floor(pages.length / 2)], 10)
+    }
+  })
+  console.log(sum)
+})

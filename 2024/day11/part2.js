613c961e3429b4149260a39150908f5fb1ffada3
Mikhail Labanov <maikudou@gmail.com>
2025-06-18 19:22:23 -0700

    2024 day 11 part 2

--- 2024/day11/part2.js
+++ 2024/day11/part2.js
@@ -2,51 +2,41 @@ var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream(__dirname + '/input')
 })
 
-let root = null
+let stones
 
 lineReader.on('line', function (line) {
   let current = null
-  line.split(' ').forEach(stone => {
-    const newStone = {
-      value: stone,
-      next: null
-    }
-    if (root === null) {
-      root = newStone
-    }
-    if (current) {
-      current.next = newStone
-    }
-    current = newStone
-  })
+  stones = line
 })
 
-lineReader.on('close', function () {
-  for (let i = 0; i < 25; i++) {
-    let current = root
-    while (current) {
-      if (current.value === '0') {
-        current.value = '1'
-      } else if (current.value.length % 2 === 0) {
-        const newStone = {
-          value: String(parseInt(current.value.substring(current.value.length / 2), 10)),
-          next: current.next
-        }
-        current.value = String(parseInt(current.value.substring(0, current.value.length / 2)))
-        current.next = newStone
-        current = newStone
-      } else {
-        current.value = String(current.value * 2024)
-      }
-
-      current = current.next
+function processStones(stonesMap) {
+  const newMap = new Map()
+  stonesMap.forEach((count, stone) => {
+    if (stone === '0') {
+      newMap.set('1', (newMap.get('1') || 0) + count)
+    } else if (stone.length % 2 === 0) {
+      const left = stone.substring(0, stone.length / 2)
+      newMap.set(left, (newMap.get(left) || 0) + count)
+      const right = String(parseInt(stone.substring(stone.length / 2)))
+      newMap.set(right, (newMap.get(right) || 0) + count)
+    } else {
+      const newStone = String(stone * 2024)
+      newMap.set(newStone, (newMap.get(newStone) || 0) + count)
     }
+  })
+  return newMap
+}
+
+lineReader.on('close', function () {
+  let stonesMap = stones.split(' ').reduce((acc, value) => {
+    const count = acc.get(value) || 0
+    acc.set(value, count + 1)
+    return acc
+  }, new Map())
+
+  for (let i = 0; i < 75; i++) {
+    stonesMap = processStones(stonesMap)
   }
-  let count = 0
-  current = root
-  while (current) {
-    count++
-    current = current.next
-  }
-  console.log(count)
+
+  console.log(Array.from(stonesMap.values()).reduce((acc, value) => acc + value, 0))
 })
